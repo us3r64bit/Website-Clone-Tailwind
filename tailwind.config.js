a4f0672092466c6aaf2d2fc2804b59bd848221cf
User64bit <76396335+us3r64bit@users.noreply.github.com>
2024-03-26 19:02:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'playfair':'Playfair Display',
+      },
+      colors:{
+        'nav':'#FFFFFF',
+        'navline':'#D0D0A7',
+        'home':'#E7F2ED',
+        'color2':'rgba(242, 242, 230, 1)',
+        'bcolor':'#0B8497',
+        'arrow':'#0B8497',
+        'txt':'#04282D',
+        'card-hover':'#E8F2EE',
+        'page6':'#E7F2ED',
+        'page7txt':'#777777',
+        'page7tr':'#808013',
+        'page7pho':'#F8A488',
+      }
+    },
+  },
+  plugins: [],
+}
